c930d074617e4c475c142c2c8f9aaf6c7994c87e
noqcks <benji@093b.org>
2024-04-04 17:00:52 +0000

    Upgrade dependencies

--- src/main/utils.ts
+++ src/main/utils.ts
@@ -1,6 +1,6 @@
 import * as JsonValidator from 'ajv'
 
-const JSON_VALIDATOR: JsonValidator.Ajv = new JsonValidator()
+const JSON_VALIDATOR = new JsonValidator()
 
 export function objectMatchesSchema(schema: object, data: any): boolean {
     return JSON_VALIDATOR.validate(schema, data) as boolean
